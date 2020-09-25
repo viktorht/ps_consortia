12d5aa351dedb3148495b2fd8f6e3129b3fa788f
Viktor Hesselberg-Thomsen <viktorht@yahoo.dk>
2020-09-25 12:42:56 +0200

    cleaning up and commenting

--- R/setup.R
+++ R/setup.R
@@ -7,5 +7,4 @@ library(purrr)
 library(here)
 
 folder_names <- c("data/raw", "data/tidy", "refs", "R", "analysis", "figures", "man")
-#sapply(folder_names, dir.create) # base R way
-map(folder_names, dir.create) #purrr-fect way
+map(folder_names, dir.create) # Creates folder 
