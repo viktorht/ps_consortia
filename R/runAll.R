412f4633b1c38957bfd8e21940100816d2a6f048
Viktor Hesselberg-Thomsen <viktorht@yahoo.dk>
2020-09-28 12:09:29 +0200

    run all file to run the other scripts

---
+++ R/runAll.R
@@ -0,0 +1,17 @@
+## Main workflow: PSconsortia
+## Constributors: viktorht@yahoo.dk
+
+## Main file to run all
+library(here)
+
+source('R/setup.R')
+
+# Data processing
+source("R/inferrence_of_degs.R") # use DESeq2 to identify DEGs
+source('R/prepare_gene_sets.R') # create kegg based gene sets from eggnog annotation
+
+# Analysis
+source('R/geneSetEnrichmentAnalysis_hyperTest.R')
+
+
+# plotting
