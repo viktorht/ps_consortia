e8c1b996e7436d6ee0b7de3ddd362cdccddbb716
Viktor Hesselberg-Thomsen <viktorht@yahoo.dk>
2020-09-28 10:17:30 +0200

    creates gene sets, fetch kegg names and saves both into separate files

--- R/prepare_gene_sets.R
+++ R/prepare_gene_sets.R
@@ -5,25 +5,26 @@ library(data.table)
 library(KEGGREST)
 source("R/custom_functions.R")
 
-bac <- 'S'
+###### Input data ##########
+bacteriaList <- c(quote(P), quote(S)) # quote is used to the make use of the bac variable to subset data.table
 updatePathwayNameFile <- TRUE
 eggnog.files <- list("S" = c("data/raw/eGGNOG_mapper/Bacillus.tsv"), 
                      "P" = c("data/raw/eGGNOG_mapper/job_MM_skhrncty_annotations_pseudomonas.tsv"))
-eggnogTable <- readEggnogTable(eggnog.files[[1]])
-geneSets <- makeKEGGGeneSets(eggnogTable = eggnogTable)
+############################
 
-
-if (updatePathwayNameFile) { # This takes a few minutes 
-  geneSetNames <- fetchKeggNames(names(geneSets)) # fetch the names of the pathways ways 
-  write.table(geneSetNames, paste0('data/raw/pathway_names_', bac, '.txt')) # save to file 
+for (bac in bacteriaList){
+  eggnogTable <- readEggnogTable(eggnog.files[[bac]])
+  geneSets <- makeKEGGGeneSets(eggnogTable = eggnogTable)
+  
+  
+  if (updatePathwayNameFile) { # This takes a few minutes 
+    geneSetNames <- fetchKeggNames(names(geneSets)) # fetch the names of the pathways ways 
+    write.table(geneSetNames, paste0('data/raw/pathway_names_', bac, '.txt')) # save to file 
+  }
+  geneSetNames <- read.table(paste0('data/raw/pathway_names_', bac, '.txt'))
+  
+  geneSets[!(names(geneSets) %in% geneSetNames$kegg.ko)] # find pathways that were not found in kegg
+  
+  saveRDS(object = geneSets, file = paste0('data/tidy/geneSets_', bac, '.rds'))
 }
-geneSetNames <- read.table(paste0('data/raw/pathway_names_', bac, '.txt'))
 
-geneSets[!(names(geneSets) %in% geneSetNames$kegg.ko)] # find pathways that were not found in kegg
-
-
-
-# Create genes sets
-# Add kegg names
-
-bacteriaList <- c(quote(P), quote(S)) # quote is used to the make use of the bac variable to subset data.table
