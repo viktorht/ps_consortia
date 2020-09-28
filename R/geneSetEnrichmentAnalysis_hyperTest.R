b2adb0cbf1b5c32c523d60b01e260edb61efa5cc
Viktor Hesselberg-Thomsen <viktorht@yahoo.dk>
2020-09-28 11:57:11 +0200

    Takes deseq2 results files and calculate hyperTest and outputs csv files with results for each strain

--- R/geneSetEnrichmentAnalysis_hyperTest.R
+++ R/geneSetEnrichmentAnalysis_hyperTest.R
@@ -3,35 +3,38 @@
 
 library(here)
 library(data.table)
+source('R/custom_functions.R')
+
+use.data = 'PS'
+bac = 'S'
 
 if (use.data == "PS"){
   eggnog.files <- list("S" = c("data/raw/eGGNOGmapper/Bacillus.tsv"), 
                        "P" = c("data/raw/eGGNOGmapper/job_MM_skhrncty_annotations_pseudomonas.tsv"))
+  dat.file <- list('P' = c('DESeq2Results_PSvsP-P_conditions_all_shrunkenLFC.csv'),
+                   'S' = c('DESeq2Results_PSvsS-S_conditions_all_shrunkenLFC.csv'))
+
+  
   outFolderName <- 'DESeq2based'
-  outsuffix <- paste0('DESeq2_PS_all_shrunkenLFC')
-  mainwd <- getwd()
   bacteriaList <- c(quote(S), quote(P)) # quote is used to the make use of the bac variable to subset data.table
 }
 
 degCountsAll <- data.table(NULL) # for collecting data
 for (bac in bacteriaList) {
   # load deseq2 results
-  dat.file <- list.files('data/clean/DESeq2/', pattern = paste0(use.data, 'vs', bac, '.*all.*shrunken'))
-  deseq2Results <- fread(input = paste0('data/clean/DESeq2/', dat.file))
+  deseq2Results <- fread(input = paste0('data/tidy/DESeq2/', dat.file[[bac]]))
   setnames(deseq2Results, 'V1', 'geneID')
   
   # Make geneSets from eggnog
-  eggnogTable <- readEggnogTable(file = eggnog.files[[bac]])
-  geneSets <- makeKEGGGeneSets(eggnogTable = eggnogTable)
+  geneSets <- readRDS(paste0('data/tidy/geneSets/geneSets_', bac, '.rds'))
   
-  # Calculate rich factor 
-  degCounts <- calcRichfactor(degseq2Results = deseq2Results, geneSets = geneSets, padjCutOff = 0.05, lfcCutOff = 1) 
-  degCounts[, Bacteria := as.character(bac)] # adds the bacteria 
+  hyperTestResults <- GeneSetHyperTest(degResults = deseq2Results, 
+                                       geneSet = geneSets, 
+                                       lfcCut = 1, 
+                                       pvalCut = 0.05)
   
-  degCounts <- merge(degCounts, GeneSetHyperTest(degResults = deseq2Results, 
-                                                 geneSet = geneSets, 
-                                                 lfcCut = 1, 
-                                                 pvalCut = 0.05))
-  
-  degCountsAll <- rbind(degCountsAll, degCounts) # collects data
+  write.csv(x = hyperTestResults, # Saves data to file
+            file = paste0('data/tidy/geneSetEnrichmentAnalysis/hyperTestResults_', bac, '.csv'),
+            row.names = F)
 }
+
