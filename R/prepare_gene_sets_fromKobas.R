9abd96d258df195f3fd4905cd41468483cfacb8e
Viktor Hesselberg-Thomsen <viktorht@yahoo.dk>
2020-09-28 15:55:53 +0200

    Investigate the gene set differences for P

--- R/prepare_gene_sets_fromKobas.R
+++ R/prepare_gene_sets_fromKobas.R
@@ -13,24 +13,23 @@ updatePathwayNameFile <- FALSE
 kobasFiles <- list("S" = c('data/raw/kobasAnnotation/sqr9.txt'), 
                     "P" = c("data/raw/kobasAnnotation/pae.txt"))
 ############################
-bac <- 'S'
+bac <- 'P'
 
 kobasAnnot <- readKobasAnnotation(file = kobasFiles[[bac]])
 
-genesAndPathways <- keggLink('bamy', 'pathway')
-refGeneSets_long <- data.table('refGeneID' = genesAndPathway, 'kegg.ko' = names(genesAndPathways))
+genesAndPathways <- keggLink('pae', 'pathway')
+refGeneSets_long <- data.table('refGeneID' = genesAndPathways, 'kegg.ko' = names(genesAndPathways))
 geneSets_long <- merge.data.table(x = kobasAnnot,
                                  y = refGeneSets_long,
                                  by.x = 'keggID',
                                  by.y = 'refGeneID')
 
 # compare the gene sets from kobas to eggnog
-geneSets_eggnog <- readRDS(file = 'data/tidy/geneSets/geneSets_S.rds')
+geneSets_eggnog <- readRDS(file = 'data/tidy/geneSets/geneSets_P.rds')
 
 # length of BCAA synthesis gene set 
-length(geneSets_long[kegg.ko == 'path:bamy00290', query]) # from kobas
+length(geneSets_long[kegg.ko == 'path:pae00290', query]) # from kobas
 length(geneSets_eggnog$ko00290) # from eggnog
-# The length is the same
 
-sum(!geneSets_long[kegg.ko == 'path:bamy00290', query] %in% geneSets_eggnog$ko00290)
-# The two method finds the exact same genes to be part of that pathway
+geneSets_eggnog$ko00290[!(geneSets_eggnog$ko00290 %in% geneSets_long[kegg.ko == 'path:pae00290', query])]
+# Three genes are not in the kobas gene set
