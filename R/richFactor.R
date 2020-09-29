4fd67de0f55ef49656377e5e9d9440cb8512b383
Viktor Hesselberg-Thomsen <viktorht@yahoo.dk>
2020-09-29 11:37:09 +0200

    compatible with kobas based gene sets

--- R/richFactor.R
+++ R/richFactor.R
@@ -3,7 +3,7 @@ library(data.table)
 source('R/custom_functions.R')
 
 use.data = 'PS'
-
+use.geneSets = 'kobas'
 if (use.data == "PS"){
   eggnog.files <- list("S" = c("data/raw/eGGNOGmapper/Bacillus.tsv"), 
                        "P" = c("data/raw/eGGNOGmapper/job_MM_skhrncty_annotations_pseudomonas.tsv"))
@@ -12,14 +12,23 @@ if (use.data == "PS"){
   bacteriaList <- c(quote(S), quote(P)) # quote is used to the make use of the bac variable to subset data.table
 }
 
-degCountsAll <- data.table(NULL) # for collecting data
+
 for (bac in bacteriaList) {
+  if (!(use.geneSets %in% c('kobas', 'eggnog'))){ # Check validity of use.geneSets
+    stop('Invalid use.geneSets selected.')
+  }
+  
+  # load selected gene sets
+  if (use.geneSets == 'kobas'){
+    geneSets <- readRDS(paste0('data/tidy/geneSets/geneSets_kobas_', bac, '.rds'))
+  }
+  if (use.geneSets == 'eggnog'){
+    geneSets <- readRDS(paste0('data/tidy/geneSets/geneSets_', bac, '.rds'))
+  }
+  
   # load deseq2 results
   deseq2Results <- fread(input = paste0('data/tidy/DESeq2/', dat.file[[bac]]))
   setnames(deseq2Results, 'V1', 'geneID')
-  
-  # Make geneSets from eggnog
-  geneSets <- readRDS(paste0('data/tidy/geneSets/geneSets_', bac, '.rds'))
   
   # Calculate rich factor 
   degCounts <- calcRichfactor(degseq2Results = deseq2Results, geneSets = geneSets, padjCutOff = 0.05, lfcCutOff = 2) 
@@ -27,6 +36,6 @@ for (bac in bacteriaList) {
 
   
   write.csv(x = degCounts, # Saves data to file
-            file = paste0('data/tidy/geneSetEnrichmentAnalysis/richFactor_', bac, '.csv'),
+            file = paste0('data/tidy/geneSetEnrichmentAnalysis/richFactor_', use.geneSets, '_', bac, '.csv'),
             row.names = F)
 }
