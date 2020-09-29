bb034c2f75f087d9a19b3179838506529a10f16a
Viktor Hesselberg-Thomsen <viktorht@yahoo.dk>
2020-09-29 10:56:51 +0200

    commenting

--- R/prepare_gene_sets_fromKobas.R
+++ R/prepare_gene_sets_fromKobas.R
@@ -12,19 +12,19 @@ bacteriaList <- c(quote(P), quote(S)) # quote is used to the make use of the bac
 updatePathwayNameFile <- FALSE
 kobasFiles <- list("S" = c('data/raw/kobasAnnotation/sqr9.txt'), 
                     "P" = c("data/raw/kobasAnnotation/pae.txt"))
-keggOrgId <- list('S' = c('bamy'),
+keggOrgId <- list('S' = c('bamy'), # the org id that was used by kobas as reference 
                   'P' = c('pae'))
 ############################
 
 for (bac in bacteriaList){
-  kobasAnnot <- readKobasAnnotation(file = kobasFiles[[bac]])
+  kobasAnnot <- readKobasAnnotation(file = kobasFiles[[bac]]) # load the kobas file
   
-  genesAndPathways <- keggLink(keggOrgId[[bac]], 'pathway')
-  refGeneSets_long <- data.table('refGeneID' = genesAndPathways, 'kegg.ko' = names(genesAndPathways))
-  geneSets_long <- merge.data.table(x = kobasAnnot,
-                                    y = refGeneSets_long,
+  genesAndPathways <- keggLink(keggOrgId[[bac]], 'pathway') # get the mapping between kegg Gene ID and pathways
+  keggGeneSets_long <- data.table('keggGeneID' = genesAndPathways, 'kegg.ko' = names(genesAndPathways)) # creates a data table of the above list
+  geneSets_long <- merge.data.table(x = kobasAnnot, # merge query IDs with keggID to map query IDs to genes sets
+                                    y = keggGeneSets_long,
                                     by.x = 'keggID',
-                                    by.y = 'refGeneID')
+                                    by.y = 'keggGeneID')
   
   # convert gene set long format to list format
   uniqueKos <- unique(geneSets_long$kegg.ko)
