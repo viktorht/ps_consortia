cc93baa6a89b381fc152e4c6b37329ead358a865
Viktor Hesselberg-Thomsen <viktorht@yahoo.dk>
2020-09-29 10:47:08 +0200

    Creates kobas gene sets

--- R/prepare_gene_sets_fromKobas.R
+++ R/prepare_gene_sets_fromKobas.R
@@ -12,24 +12,24 @@ bacteriaList <- c(quote(P), quote(S)) # quote is used to the make use of the bac
 updatePathwayNameFile <- FALSE
 kobasFiles <- list("S" = c('data/raw/kobasAnnotation/sqr9.txt'), 
                     "P" = c("data/raw/kobasAnnotation/pae.txt"))
+keggOrgId <- list('S' = c('bamy'),
+                  'P' = c('pae'))
 ############################
-bac <- 'P'
 
-kobasAnnot <- readKobasAnnotation(file = kobasFiles[[bac]])
+for (bac in bacteriaList){
+  kobasAnnot <- readKobasAnnotation(file = kobasFiles[[bac]])
+  
+  genesAndPathways <- keggLink(keggOrgId[[bac]], 'pathway')
+  refGeneSets_long <- data.table('refGeneID' = genesAndPathways, 'kegg.ko' = names(genesAndPathways))
+  geneSets_long <- merge.data.table(x = kobasAnnot,
+                                    y = refGeneSets_long,
+                                    by.x = 'keggID',
+                                    by.y = 'refGeneID')
+  
+  # convert gene set long format to list format
+  uniqueKos <- unique(geneSets_long$kegg.ko)
+  geneSets <- sapply(uniqueKos, function(ko){geneSets_long[kegg.ko == ko, query]})
+  
+  saveRDS(geneSets, file = paste0('data/tidy/geneSets/geneSets_Kobas_', bac, '.rds'))
+}
 
-genesAndPathways <- keggLink('pae', 'pathway')
-refGeneSets_long <- data.table('refGeneID' = genesAndPathways, 'kegg.ko' = names(genesAndPathways))
-geneSets_long <- merge.data.table(x = kobasAnnot,
-                                 y = refGeneSets_long,
-                                 by.x = 'keggID',
-                                 by.y = 'refGeneID')
-
-# compare the gene sets from kobas to eggnog
-geneSets_eggnog <- readRDS(file = 'data/tidy/geneSets/geneSets_P.rds')
-
-# length of BCAA synthesis gene set 
-length(geneSets_long[kegg.ko == 'path:pae00290', query]) # from kobas
-length(geneSets_eggnog$ko00290) # from eggnog
-
-geneSets_eggnog$ko00290[!(geneSets_eggnog$ko00290 %in% geneSets_long[kegg.ko == 'path:pae00290', query])]
-# Three genes are not in the kobas gene set
