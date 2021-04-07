1ff215480f21d37544aa660bf65f17cd5d9d37a4
Viktor Hesselberg-Thomsen <viktorht@yahoo.dk>
2021-04-07 23:13:06 +0200

    change use.genesets to eggnog

--- R/richFactor.R
+++ R/richFactor.R
@@ -3,7 +3,7 @@ library(data.table)
 source('R/custom_functions.R')
 
 use.data = 'PS'
-use.geneSets = 'kobas'
+use.geneSets = 'eggnog'
 if (use.data == "PS"){
   eggnog.files <- list("S" = c("data/raw/eGGNOGmapper/Bacillus.tsv"), 
                        "P" = c("data/raw/eGGNOGmapper/job_MM_skhrncty_annotations_pseudomonas.tsv"))
